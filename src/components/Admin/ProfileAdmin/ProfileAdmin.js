30a44c5c7a31575f0b6c4fa437ad5cdd1b9ce1e7
Jack <jackdijkema@hotmail.com>
2022-12-07 15:03:02 +0100

    FEAT: added labels, added github and linkedin links

--- src/components/Admin/ProfileAdmin/ProfileAdmin.js
+++ src/components/Admin/ProfileAdmin/ProfileAdmin.js
@@ -31,17 +31,23 @@ const ProfileAdmin = () => {
   const enteredNameRef = useRef();
   const enteredOccupationRef = useRef();
   const enteredBioRef = useRef();
+  const enteredGithubRef = useRef();
+  const enteredLinkedinRef = useRef();
 
   const editProfileHandler = async () => {
     const enteredName = enteredNameRef.current.value;
     const enteredOccupation = enteredOccupationRef.current.value;
     const enteredBio = enteredBioRef.current.value;
+    const enteredGithub = enteredGithubRef.current.value;
+    const enteredLinkedin = enteredLinkedinRef.current.value;
 
     try {
       const docRef = await setDoc(doc(db, "profile", profile[0]?.id), {
         name: enteredName,
         occupation: enteredOccupation,
         bio: enteredBio,
+        github: enteredGithub,
+        linkedin: enteredLinkedin,
       });
       FetchProfile();
     } catch (error) {
@@ -57,22 +63,37 @@ const ProfileAdmin = () => {
     <section className="profile_container">
       <h2 className="profile__h1">Profile</h2>
       <section className="profile_preview">
+        <label>Name</label>
         <input
           ref={enteredNameRef}
           type="text"
           defaultValue={profile[0]?.name}
         ></input>
+        <label>Occupation</label>
         <input
           ref={enteredOccupationRef}
           type="text"
           defaultValue={profile[0]?.occupation}
         ></input>
+        <label>Bio</label>
         <textarea
           ref={enteredBioRef}
           className="profile_textarea"
           type="text"
           defaultValue={profile[0]?.bio}
         ></textarea>
+        <label>Github</label>
+        <input
+          ref={enteredGithubRef}
+          type="text"
+          defaultValue={profile[0]?.github}
+        ></input>
+        <label>LinkedIn</label>
+        <input
+          ref={enteredLinkedinRef}
+          type="text"
+          defaultValue={profile[0]?.linkedin}
+        ></input>
       </section>
       <section className="profile_edit">
         <button className="button" onClick={editProfileHandler}>
