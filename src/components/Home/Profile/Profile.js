695e14b0355c65064ce1fb953f9f3583817aa327
Jack <jackdijkema@hotmail.com>
2022-12-09 12:43:01 +0100

    display data from db

--- src/components/Home/Profile/Profile.js
+++ src/components/Home/Profile/Profile.js
@@ -2,10 +2,37 @@ import "./Profile.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { db } from "../../../store/firebase";
+import { useEffect, useState } from "react";
+import {
+  collection,
+  getDocs,
+} from "firebase/firestore";
 library.add(faGithub);
 library.add(faLinkedin);
 
 function Profile(props) {
+
+  const [profile, setProfile] = useState([]);
+
+  const FetchProfile = async () => {
+    try {
+      getDocs(collection(db, "profile")).then((querySnapshot) => {
+        const newData = querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setProfile(newData);
+        console.log(newData);
+      });
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    FetchProfile();
+  }, []);
   return (
     <section className="profile">
       <figure className="profile_figure">
@@ -15,24 +42,24 @@ function Profile(props) {
           alt="selfie of {props.name}"
         ></img>
       </figure>
-      <h1 className="profile_name">{props.name}</h1>
-      <h2 className="profile_title">{props.title}</h2>
+      <h1 className="profile_name">{profile[0]?.name}</h1>
+      <h2 className="profile_title">{profile[0]?.occupation}</h2>
 
       <ul className="profile_socials">
         <div className="profile_socials_link">
-          <a href="https://github.com/jackdijkema">
+          <a href={profile[0]?.github}>
             <FontAwesomeIcon icon="fa-brands fa-github" size="1x" />
           </a>
         </div>
         <div className="profile_socials_link">
-          <a href="https://google.com">
+          <a href={profile[0]?.linkedin}>
             <FontAwesomeIcon icon="fa-brands fa-linkedin" size="1x" />
           </a>
         </div>
       </ul>
 
       <div className="profile_about">
-        <p className="about">{props.about}</p>
+        <p className="about">{profile[0]?.bio}</p>
       </div>
     </section>
   );
