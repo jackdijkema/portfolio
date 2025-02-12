7352465201583004c1d3e8c35ea0e174b292b30e
Jack <jackdijkema@hotmail.com>
2025-02-12 21:18:21 +0100

    undo attempt, remove vercel

--- src/store/firebase.js
+++ src/store/firebase.js
@@ -1,8 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -12,8 +10,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig, {
-  experimentalForceLongPolling: true,
-});
+const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
